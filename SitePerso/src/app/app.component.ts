534a311577ac53b48dd8811990382a40fcb205ca
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2025-12-01 00:41:48 +0100

    Add New Cv + dropdown menu on Mobile

--- SitePerso/src/app/app.component.ts
+++ SitePerso/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,6 +9,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'SitePerso';
+
+  isMenuOpen = signal(false);
+  menuIcon = computed(() => this.isMenuOpen() ? '✕' : '☰');
+
+  toggleMenu() {
+    this.isMenuOpen.update(current => !current);
+  }
 
   onClickCV() {
     window.open('cv.pdf', '_self')
