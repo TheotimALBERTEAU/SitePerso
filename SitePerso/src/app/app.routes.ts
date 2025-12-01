0e961dfb31255d09196f669160bc76c0a858e156
Théotim ALBERTEAU <sunshouwu@outlook.com>
2025-12-01 13:31:52 +0100

    Add Contact + working contact form

--- SitePerso/src/app/app.routes.ts
+++ SitePerso/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {AppComponent} from './app.component';
 import {MainPageComponent} from './pages/main-page/main-page.component';
+import {ContactComponent} from './pages/contact/contact.component';
 
 export const routes: Routes = [
   { path : "TheotimAlberteau", component : MainPageComponent },
   { path : "", component : MainPageComponent },
+  { path : "Contact", component : ContactComponent },
 ];
