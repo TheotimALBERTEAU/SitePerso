9129d4e9a24cedb0cb4b49ae55a8e1dd09dae7f3
Théotim ALBERTEAU <sunshouwu@outlook.com>
2026-03-09 10:00:50 +0100

    Add Legal Notices Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {AppComponent} from './app.component';
 import {MainPageComponent} from './pages/main-page/main-page.component';
+import {LegalNoticesComponent} from './pages/legal-notices/legal-notices.component';
 
 export const routes: Routes = [
   { path : "TheotimAlberteau", component : MainPageComponent },
   { path : "", component : MainPageComponent },
+  { path : "LegalNotices", component : LegalNoticesComponent },
 ];
